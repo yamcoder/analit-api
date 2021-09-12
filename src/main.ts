6ab7e7ae5ca83a030d9380b5354781f0c7acd14a
Yulian Mukhamedov <imukhamedov@neoflex.ru>
2021-09-12 20:17:03 +0300

    port from env

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
+const PORT = process.env.PORT || 3000;
+
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.setGlobalPrefix('api');
 	app.useGlobalPipes(new ValidationPipe());
-	await app.listen(3000);
+	await app.listen(PORT, () =>
+		console.log(`Server is running on port ${PORT}`),
+	);
 }
 bootstrap();
